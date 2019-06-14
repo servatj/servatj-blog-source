864b58c4b272385fe4774612d3b5a0b865374751
Josep Servat <jservatlorca@gmail.com>
2019-06-14 16:25:21 +0200

    initial layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,60 @@
-import React from 'react'
-import LayoutComponent from "../components/Layout"
+import React from "react"
+import Header from "../components/Header"
+import { graphql, Link } from "gatsby"
 
 const Layout = ({ data }) => {
-  console.log(data)
-   return  (<Layout
-     data={data}
-   />)
+  const { edges } = data.allMarkdownRemark
+  return (
+    <div>
+      <Header />
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        fontFamily: 'avenir'
+      }}>
+        {edges.map(edge => {
+          const {frontmatter} = edge.node
+          return (
+            <div
+              key={frontmatter.path}
+              style={{marginBottom: '1rem'}}
+            >
+              <Link
+                style={{
+                  boxShadow: 'none',
+                  textDecoration: 'none',
+                  color: 'var(--textTitle)',
+                }}
+                to={frontmatter.path}>
+                {frontmatter.title}
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
-export default Layout
+export const query = graphql`
+  query HomepageQuery {
+    allMarkdownRemark {
+      edges {
+        node{
+          frontmatter{
+            title
+            path
+            date
+          }
+        }
+      }
+    }
+  }
+`
+
+const init = ({ data }) => {
+  return <Layout data={data} />
+}
+
+export default init
