6c6f205567aff5de437639d6aa525979e83b7a18
Josep Servat <jservatlorca@gmail.com>
2019-06-14 16:55:46 +0200

    Layout second part

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,6 @@
 import React from "react"
-import Header from "../components/Header"
-import { graphql, Link } from "gatsby"
-
-const Layout = ({ data }) => {
-  const { edges } = data.allMarkdownRemark
-  return (
-    <div>
-      <Header />
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        fontFamily: 'avenir'
-      }}>
-        {edges.map(edge => {
-          const {frontmatter} = edge.node
-          return (
-            <div
-              key={frontmatter.path}
-              style={{marginBottom: '1rem'}}
-            >
-              <Link
-                style={{
-                  boxShadow: 'none',
-                  textDecoration: 'none',
-                  color: 'var(--textTitle)',
-                }}
-                to={frontmatter.path}>
-                {frontmatter.title}
-              </Link>
-            </div>
-          )
-        })}
-      </div>
-    </div>
-  )
-}
+import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
 export const query = graphql`
   query HomepageQuery {
@@ -53,8 +18,6 @@ export const query = graphql`
   }
 `
 
-const init = ({ data }) => {
+export default ({ data }) => {
   return <Layout data={data} />
 }
-
-export default init
