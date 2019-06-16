f263318d854613ef4181941374d3cb94137e171e
Josep Servat <jservatlorca@gmail.com>
2019-06-17 01:44:45 +0200

    Test Header

---
+++ src/components/__tests__/header.js
@@ -0,0 +1,23 @@
+import React from "react"
+import renderer from "react-test-renderer"
+import { StaticQuery } from "gatsby"
+import Header from "../Header"
+
+beforeEach(() => {
+  StaticQuery.mockImplementationOnce(({ render }) =>
+    render({
+      site: {
+        siteMetadata: {
+          title: `Default Starter`,
+        },
+      },
+    })
+  )
+})
+
+describe("Header", () => {
+  it("renders correctly", () => {
+    const tree = renderer.create(<Header />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
