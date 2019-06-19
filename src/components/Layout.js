aed416e5f6bde636a84ac9481d02e07d4f931b8d
Josep Servat <jservatlorca@gmail.com>
2019-06-19 19:34:05 +0200

    Add some linting

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,40 +1,46 @@
-import React from "react";
-import Header from "../components/Header";
-import { Link } from "gatsby";
+/* eslint-disable import/first */
+import React from 'react'
+import Header from '../components/Header'
+import {Link} from 'gatsby'
+import {object} from 'prop-types'
 
-const Layout = ({ data }) => {
-  const { edges } = data.allMarkdownRemark;
+const Layout = ({data}) => {
+  const {edges} = data.allMarkdownRemark
   return (
     <div>
       <Header />
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          fontFamily: "avenir"
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          fontFamily: 'avenir',
         }}
       >
         {edges.map(edge => {
-          const { frontmatter } = edge.node;
+          const {frontmatter} = edge.node
           return (
-            <div key={frontmatter.path} style={{ marginBottom: "1rem" }}>
+            <div key={frontmatter.path} style={{marginBottom: '1rem'}}>
               <Link
                 style={{
-                  boxShadow: "none",
-                  textDecoration: "none",
-                  color: "var(--textTitle)"
+                  boxShadow: 'none',
+                  textDecoration: 'none',
+                  color: 'var(--textTitle)',
                 }}
                 to={frontmatter.path}
               >
                 {frontmatter.title}
               </Link>
             </div>
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Layout;
+Layout.propTypes = {
+  data: object,
+}
+
+export default Layout
