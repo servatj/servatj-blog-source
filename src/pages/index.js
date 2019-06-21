def9e621032e8249813dc1cc4937c325fd8dadee
Josep Servat <jservatlorca@gmail.com>
2019-06-21 11:16:19 +0200

    Remove Layout component and 404

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,44 @@
-import React from "react";
-import Layout from "../components/Layout";
-import { graphql } from "gatsby";
+/* eslint-disable import/first */
+import React from 'react'
+import Header from '../components/Header'
+import {graphql, Link} from 'gatsby'
+
+import '../utils/global.css'
+
+// eslint-disable-next-line react/prop-types
+const Layout = ({data}) => {
+  const {edges} = data.allMarkdownRemark
+  return (
+    <div>
+      <Header />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {edges.map(edge => {
+          const {frontmatter} = edge.node
+          return (
+            <div key={frontmatter.path} style={{marginBottom: '1rem'}}>
+              <Link
+                style={{
+                  boxShadow: 'none',
+                  textDecoration: 'none',
+                  color: '#fff',
+                }}
+                to={frontmatter.path}
+              >
+                {frontmatter.title}
+              </Link>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
 
 export const query = graphql`
   query HomepageQuery {
@@ -16,8 +54,6 @@ export const query = graphql`
       }
     }
   }
-`;
+`
 
-export default ({ data }) => {
-  return <Layout data={data} />;
-};
+export default Layout
